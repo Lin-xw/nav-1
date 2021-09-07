428ad28e96de22d05189ca1c592c6c27b1db351e
wayy <948840255@qq.com>
2021-09-07 16:27:58 +0800

    修改网站地址

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const $lastLi = $siteList.find("li.last"); //然后再找到li.last然后插入
 const x = localStorage.getItem("x");
 const xObject = JSON.parse(x); //把字符串重新变成对象
 const hashMap = xObject || [
-    {logo: "A", url: "https://www.acfun.con",},
+    {logo: "A", url: "https://www.acfun.cn/",},
     {logo: "B", url: "https://www.bilibili.com/",},
     {logo: "C", url: "https://cc.163.com/",},
     {logo: "D", url: "https://www.douyu.com/",},
